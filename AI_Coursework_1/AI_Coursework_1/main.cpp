88d67254c0ef4f3b4bce62d6f5deba3a9ad704ee
Joseph Roper <1901881@uad.ac.uk>
2022-03-17 15:51:59 +0000

    Finished ImGui Set up

--- AI_Coursework_1/AI_Coursework_1/main.cpp
+++ AI_Coursework_1/AI_Coursework_1/main.cpp
@@ -8,8 +8,13 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Window Title");
 	ImGui::SFML::Init(window);
 
-	sf::CircleShape shape(200.0f, 100);
-	shape.setFillColor(sf::Color(204, 77, 5));//Color Circle
+	bool circleExists = true;
+	float circleRadius = 200.0f;
+	int circleSegments = 100;
+	float circleColor[3] = { (float)204 / 255, (float)77 / 255, (float)5 / 255, };
+	sf::CircleShape shape(circleRadius, circleSegments);
+	
+	shape.setFillColor(sf::Color((int)(circleColor[0] * 255), (int)(circleColor[1] * 255), (int)(circleColor[2] * 255)));//Color Circle
 	shape.setPosition(200, 200); //Center Circle
 
 	sf::Clock deltaClock;
@@ -28,10 +33,21 @@ int main()
 
 			ImGui::Begin("Window title");
 			ImGui::Text("Window text!");
+			ImGui::Checkbox("Circle", &circleExists);
+			ImGui::SliderFloat("Radius", &circleRadius, 100.0f, 300.0f);
+			ImGui::SliderInt("Segments", &circleSegments, 3.0f, 150.0f);
+			ImGui::ColorEdit3("Color", circleColor);
 			ImGui::End();
 
+			shape.setRadius(circleRadius);
+			shape.setPointCount(circleSegments);
+			shape.setFillColor(sf::Color((int)(circleColor[0] * 255), (int)(circleColor[1] * 255), (int)(circleColor[2] * 255)));//Color Circle
+
 			window.clear(sf::Color(18, 33, 43)); //Color Background
-			window.draw(shape);
+			if (circleExists)
+			{
+				window.draw(shape);
+			}
 			ImGui::SFML::Render(window);//Make sure ImGui is the last to render
 			window.display();
 		}
